d06c702ee839372dd5878301baca1a99a0314b87
dabl-os <77790652+dabl-os@users.noreply.github.com>
2025-06-12 01:29:05 +0200

    Add files via upload
    start

---
+++ config.py
@@ -0,0 +1,7 @@
+﻿BOT_TOKEN = "8182280992:AAFOQA_YgX8o9EXjEJqTMr5-imz92xt-kD8"  # токен ботика
+CHANNEL_URL = "https://t.me/+-orf6imR5M4zNjU0"  # ссылка канала проверка подписки
+CHANNEL_ID = "-1002619517298"  # айди канала для проверки подписки
+VERIF_CHANNEL_ID = "-1002795486640"  # айди чата для верификации(обсуждение канала с постбеками)
+SUPP = "https://t.me/sylex_manager"
+ADMIN_ID = 7435953633 # Ваш айди без скобочек
+PROMO = "SYGNAL" # промокод
